68adeb8704ad1e5ab7af91e5df2366f2875616a4
VIJAYaraaghavan Manoharan <AM.Vijay@gmail.com>
2022-12-24 12:50:01 +0000

    markdown service implementation

--- src/markdown.service.ts
+++ src/markdown.service.ts
@@ -5,7 +5,7 @@ export class MarkdownService{
 
     public async fetchContent(){
 
-        const {data, status} = await axios.get("https://github.com/AMVijay/aws-learning/blob/main/vpc/README.md");
+        const {data, status} = await axios.get("https://raw.githubusercontent.com/AMVijay/aws-learning/main/vpc/README.md");
         console.log("Data and Status", data, status);
         return data;
     }
