f4047eb2653a323afa00437c6d206a1d354d4823
VIJAYaraaghavan Manoharan <AM.Vijay@gmail.com>
2022-12-25 04:46:56 +0000

    markdown content rendering implementation

--- src/markdown.service.ts
+++ src/markdown.service.ts
@@ -8,9 +8,9 @@ export class MarkdownService{
         const blogTag = document.getElementsByTagName("blog")[0];
         console.log("blogTag ", blogTag);
 
-        const {data, status} = await axios.get("https://raw.githubusercontent.com/AMVijay/aws-learning/main/vpc/README.md");
-        console.log("Data and Status", data, status);
-        return data;
+        // const {data, status} = await axios.get("https://raw.githubusercontent.com/AMVijay/aws-learning/main/vpc/README.md");
+        // console.log("Data and Status", data, status);
+        // return data;
     }
 
 
