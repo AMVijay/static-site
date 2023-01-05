c3b1de8083b656cd5f5d910e071238f6370aefef
VIJAYaraaghavan Manoharan <AM.Vijay@gmail.com>
2023-01-05 13:56:08 +0000

    implementing static html generation

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,32 @@
-import { MarkdownService } from "./markdown.service";
-import { TitleService } from "./title.service";
 
-const markdownService : MarkdownService = new MarkdownService();
-markdownService.fetchContent();
+import * as fs from 'fs';
+import * as path from 'path';
+import { BlogInterface } from './blog.interface';
 
-const titleService: TitleService = new TitleService();
-titleService.updateTitle();
+const blog : BlogInterface = {
+    title: "test",
+    content: "test"
+};
+
+const htmlTemplate = `
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>${blog.title}</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <meta name="author" content="VIJAYaraaghavan Manoharan">
+    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+</head>
+<body>
+   <main>${blog.content}</main>
+</body>
+</html>
+`;
+
+const blogDir = path.join(__dirname, '/dist/',blog.title);
+fs.mkdirSync(blogDir);
+
  
 
